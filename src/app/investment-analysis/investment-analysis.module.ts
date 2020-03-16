ab80814a7bb0fe8c6f7a8a4e6e3dbfcf153019e8
Forstie <Ryan.Forstie1@T-Mobile.com>
2020-03-16 14:02:33 -0700

    Setup modules

---
+++ src/app/investment-analysis/investment-analysis.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InvestmentAnalysisRoutingModule } from './investment-analysis-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    InvestmentAnalysisRoutingModule
+  ]
+})
+export class InvestmentAnalysisModule { }
---
+++ src/app/trends/trends.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TrendsRoutingModule } from './trends-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TrendsRoutingModule
+  ]
+})
+export class TrendsModule { }
